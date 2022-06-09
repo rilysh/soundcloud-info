a6a89ead109a3d9117026060de4145003dfa3154
Ruzie <luhamailbox@gmail.com>
2022-06-09 14:07:56 +0530

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import SCAppVersion from "./lib/methods/AppVersion";
+import PlaylistInfo from "./lib/methods/PlaylistInfo";
+import SCSearch from "./lib/methods/Search";
+import TrackInfo from "./lib/methods/TrackInfo";
+import UserInfo from "./lib/methods/UserInfo";
+
+export {
+    SCAppVersion,
+    PlaylistInfo,
+    SCSearch,
+    TrackInfo,
+    UserInfo,
+};
