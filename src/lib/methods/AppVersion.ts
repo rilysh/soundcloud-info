904db60a6515ee8887c3dd6cc49f19d9cb4deede
Ruzie <luhamailbox@gmail.com>
2022-06-11 13:53:45 -0400

    fix(return): return a promise string

--- src/lib/methods/AppVersion.ts
+++ src/lib/methods/AppVersion.ts
@@ -1,7 +1,7 @@
 import { fetchText } from "../gate/Util"
 import { SOUNDCLOUD_URL } from "../gate/Constants"
 
-export default async function SCAppVersion() {
+export default async function SCAppVersion(): Promise<string> {
     const data = await fetchText(`${SOUNDCLOUD_URL}/versions.json`);
     const json = JSON.parse(data);
     if (json.app) {
